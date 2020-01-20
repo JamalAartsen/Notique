fb6847b50d8704724e8f618b13c6f181a57dd74a
Jamal <jamal@nakko.com>
2020-01-20 11:42:28 +0100

    Add working room database.

--- app/src/main/java/com/example/quicknote/MainActivity.kt
+++ app/src/main/java/com/example/quicknote/MainActivity.kt
@@ -1,25 +1,71 @@
 package com.example.quicknote
 
 import android.os.Bundle
-import com.google.android.material.snackbar.Snackbar
-import androidx.appcompat.app.AppCompatActivity
+import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
+import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.GridLayoutManager
-import androidx.recyclerview.widget.RecyclerView
-
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.google.android.material.snackbar.Snackbar
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.content_main.*
+import java.util.concurrent.Executor
+import java.util.concurrent.Executors
 
 class MainActivity : AppCompatActivity() {
+
+    private var notes: List<Note> = ArrayList()
+    var db = NoteRoomDatabase
+    private val executor: Executor =
+        Executors.newSingleThreadExecutor()
+    private var noteAdapter: NoteAdapter? = null
+    var count = 3
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
 
+        db.getDatabase(this)
+
+        recyclerView.layoutManager = LinearLayoutManager(this)
+        recyclerView.layoutManager = GridLayoutManager(this, 2)
+        recyclerView.adapter = NoteAdapter(notes, this)
+
         fab.setOnClickListener { view ->
+            count++
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                 .setAction("Action", null).show()
+            insertNotes(Note(count, "Hello", "Ik ben Jamal"))
+            Log.d("Count", "$count")
+        }
+
+        getAllNotes()
+    }
+
+    fun getAllNotes() {
+        executor.execute {
+            notes = db.getDatabase(this).noteDao().getAllNotes()
+            runOnUiThread {
+                updateUI()
+            }
+        }
+    }
+
+    fun insertNotes(note: Note) {
+        executor.execute {
+            db.getDatabase(this).noteDao().insert(note)
+            getAllNotes()
+        }
+    }
+
+    private fun updateUI() {
+        if (noteAdapter == null) {
+            noteAdapter = NoteAdapter(notes, applicationContext)
+            recyclerView.adapter = noteAdapter
+        } else {
+            noteAdapter?.swapList(notes)
         }
     }
 
---
+++ app/src/main/java/com/example/quicknote/NoteAdapter.kt
@@ -0,0 +1,38 @@
+package com.example.quicknote
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import kotlinx.android.synthetic.main.notes_row.view.*
+
+class NoteAdapter(var notes: List<Note>, val context: Context) :
+    RecyclerView.Adapter<NoteAdapter.ViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        return ViewHolder(LayoutInflater.from(context).inflate(R.layout.notes_row, parent, false))
+    }
+
+    override fun getItemCount(): Int {
+        return notes.size
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val note: Note = notes[position]
+
+        holder.noteTitle?.text = note.titleNote
+        holder.noteDescription?.text = note.descriptionNote
+    }
+
+    fun swapList(newList: List<Note>) {
+        notes = newList
+        notifyDataSetChanged()
+    }
+
+    class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        val noteTitle: TextView? = view.titleNote
+        val noteDescription: TextView? = view.descriptionNote
+    }
+}
