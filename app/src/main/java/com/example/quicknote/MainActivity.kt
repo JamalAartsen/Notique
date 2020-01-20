5ff2ddb8026cd9b31cd63967255c7322df271bae
Jamal <jamal@nakko.com>
2020-01-20 11:53:56 +0100

    Een snelle delete functie toegevoegd.

--- app/src/main/java/com/example/quicknote/MainActivity.kt
+++ app/src/main/java/com/example/quicknote/MainActivity.kt
@@ -2,24 +2,29 @@ package com.example.quicknote
 
 import android.os.Bundle
 import android.util.Log
+import android.view.GestureDetector
+import android.view.GestureDetector.SimpleOnGestureListener
 import android.view.Menu
 import android.view.MenuItem
+import android.view.MotionEvent
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.google.android.material.snackbar.Snackbar
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.content_main.*
 import java.util.concurrent.Executor
 import java.util.concurrent.Executors
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), RecyclerView.OnItemTouchListener {
 
     private var notes: List<Note> = ArrayList()
     var db = NoteRoomDatabase
     private val executor: Executor =
         Executors.newSingleThreadExecutor()
     private var noteAdapter: NoteAdapter? = null
+    private var mGestureDetector: GestureDetector? = null
     var count = 3
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -37,10 +42,23 @@ class MainActivity : AppCompatActivity() {
             count++
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                 .setAction("Action", null).show()
-            insertNotes(Note(count, "Hello", "Ik ben Jamal"))
+            insertNote(Note(count, "Hello", "Ik ben Jamal"))
             Log.d("Count", "$count")
         }
 
+        //Delete item with long click on the item
+        mGestureDetector = GestureDetector(this, object : SimpleOnGestureListener() {
+            override fun onLongPress(e: MotionEvent) {
+                super.onLongPress(e)
+                val child = recyclerView.findChildViewUnder(e.x, e.y)
+                if (child != null) {
+                    val adapterPosition = recyclerView.getChildAdapterPosition(child)
+                    deleteNote(notes[adapterPosition])
+                }
+            }
+        })
+
+        recyclerView.addOnItemTouchListener(this)
         getAllNotes()
     }
 
@@ -53,7 +71,7 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    fun insertNotes(note: Note) {
+    fun insertNote(note: Note) {
         executor.execute {
             db.getDatabase(this).noteDao().insert(note)
             getAllNotes()
@@ -66,6 +84,13 @@ class MainActivity : AppCompatActivity() {
             recyclerView.adapter = noteAdapter
         } else {
             noteAdapter?.swapList(notes)
+        }
+    }
+
+    fun deleteNote(note: Note) {
+        executor.execute {
+            db.getDatabase(this).noteDao().delete(note)
+            getAllNotes()
         }
     }
 
@@ -83,5 +108,18 @@ class MainActivity : AppCompatActivity() {
             R.id.action_settings -> true
             else -> super.onOptionsItemSelected(item)
         }
+    }
+
+    override fun onTouchEvent(rv: RecyclerView, e: MotionEvent) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun onInterceptTouchEvent(rv: RecyclerView, e: MotionEvent): Boolean {
+        mGestureDetector?.onTouchEvent(e)
+        return false
+    }
+
+    override fun onRequestDisallowInterceptTouchEvent(disallowIntercept: Boolean) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
 }
